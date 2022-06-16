035fb8944e2a86070c930ff4635a94bef57c1a5a
rb20180 <ruchit.bhardwaj@zs.com>
2022-06-16 12:19:50 -0500

    Created react-app-1 | React Basics and working with components

---
+++ src/components/Expenses/ExpenseItem.js
@@ -0,0 +1,18 @@
+import Card from "../UI/Card";
+import ExpenseDate from "../Expenses/ExpenseDate";
+import "./ExpenseItem.css";
+import React from "react";
+
+const ExpenseItem = (props) => {
+  return (
+    <Card className="expense-item">
+      <ExpenseDate date={props.date} />
+      <div className="expense-item__description">
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
+      </div>
+    </Card>
+  );
+}
+
+export default ExpenseItem;
